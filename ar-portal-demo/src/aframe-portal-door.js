5a9e9f73cf43e907a5ff0edade65d666d1cad1d5
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2018-05-30 15:00:02 +0200

    trying portal demo

--- ar-portal-demo/src/aframe-portal-door.js
+++ ar-portal-demo/src/aframe-portal-door.js
@@ -2,43 +2,42 @@
 //		arjs-hit-testing
 //////////////////////////////////////////////////////////////////////////////
 AFRAME.registerComponent('arjs-portal-door', {
-    schema: {
-        url: { // Url of the content - may be video or image
-            type: 'string',
-        },
-        doorWidth: { // width of the door
-            type: 'number',
-            default: 1,
-        },
-        doorHeight: { // height of the door
-            type: 'number',
-            default: 2,
-        },
-    },
-    init: () => {
-        var _this = this;
+	schema: {
+		url : {		// Url of the content - may be video or image
+			type: 'string',
+		},
+		doorWidth : {	// width of the door
+			type: 'number',
+			default: 1,
+		},
+		doorHeight : {	// height of the door
+			type: 'number',
+			default: 2,
+		},
+	},
+	init: function () {
+		var _this = this
 
-        let doorWidth = _this.data.doorWidth;
-        let doorHeight = _this.data.doorHeight;
-        let imageURL = _this.data.url;
+		var doorWidth = this.data.doorWidth
+		var doorHeight = this.data.doorHeight
+		var imageURL = this.data.url
 
-        let portalDoor = new THREEx.Portal360(imageURL, doorWidth, doorHeight);
-        _this._portalDoor = portalDoor;
+		var portalDoor = new THREEx.Portal360(imageURL, doorWidth, doorHeight)
+		this._portalDoor = portalDoor
 
-        _this.el.object3D.add(portalDoor.object3d);
-    },
-    tick: () => {
-        _this._portalDoor.update();
-    },
-});
+		this.el.object3D.add(portalDoor.object3d)
+	},
+	tick: function(){
+		this._portalDoor.update()
+	}
+})
 
 
 AFRAME.registerPrimitive('a-portal-door', AFRAME.utils.extendDeep({}, AFRAME.primitives.getMeshMixin(), {
-    defaultComponents: {
-        'arjs-portal-door': {},
-    },
-    mappings: {
-        url: 'arjs-portal-door.url',
-    },
-}));
-
+	defaultComponents: {
+		'arjs-portal-door': {},
+	},
+	mappings: {
+		'url': 'arjs-portal-door.url',
+	}
+}))
